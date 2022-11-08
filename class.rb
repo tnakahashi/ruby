6a99c1756f07238d6e85e278fbd0936d4ad3c5d9
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2022-11-08 03:08:58 +0000

    [Finish]Ch09完了

---
+++ class.rb
@@ -0,0 +1,10 @@
+array = [1,2,3]
+puts array.class
+puts array.join("*")
+
+string = "Hello"
+puts string.class
+puts string.join("*")
+
+# joinメソッドは、指定した文字列を間に挟んで連結した文字列を返す。
+#Stringクラスはjoinメソッドを使えない。
---
+++ new.rb
@@ -0,0 +1,30 @@
+class Car
+  
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  
+  def run(instance)
+    puts "車で#{instance}キロ走ります。"
+  end
+
+  def turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+car = Car.new
+car.run(5)
+
+#car = Car.new
+car.turn("右")
+car.move("右", 5)
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
